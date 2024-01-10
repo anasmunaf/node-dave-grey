873644df6a03e18b281cb7a8e087a81c5ca2a406
anasmunaf <anasmunaf@brandver.se>
2024-01-10 14:49:23 +0500

    http node server completed

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const { readFileSync, existsSync } = require("fs");
-const http = require("http");
-const path = require("path");
-const { EventEmitter } = require("stream");
+const { readFileSync, existsSync } = require("fs")
+const http = require("http")
+const path = require("path")
+const { EventEmitter } = require("stream")
 const {
-  logs: { logEvents },
-} = require("./modules/log_events");
+  logs: { logEvents }
+} = require("./modules/log_events")
 
 class Emitter extends EventEmitter {}
 
-const emitter = new Emitter();
+const emitter = new Emitter()
 
-const port = process.env.port || 3000;
+emitter.on("log", (msg, fileName) => {
+  logEvents(msg, fileName)
+})
 
-const filePath = path.join(__dirname, "views", "index.html");
+const port = process.env.port || 3000
+
+const filePath = path.join(__dirname, "views", "index.html")
 
 const ext = {
   css: "text/css",
@@ -21,78 +25,74 @@ const ext = {
   jpg: "image/jpeg",
   png: "image/png",
   txt: "text/plain",
-  html: "text/html",
-};
+  html: "text/html"
+}
 
 function serverFile(fsPath, type, res) {
   try {
     const rawData = readFileSync(fsPath, {
-      encoding: "utf8",
-    });
-    const data = type === ext.json ? JSON.parse(rawData) : rawData;
+      encoding: "utf8"
+    })
+    const data = type === ext.json ? JSON.parse(rawData) : rawData
 
     res.writeHead(fsPath.includes("404.html") ? 400 : 200, {
-      "Content-Type": type,
-    });
-    res.end(type === ext.json ? JSON.stringify(data) : data);
+      "Content-Type": type
+    })
+    res.end(type === ext.json ? JSON.stringify(data) : data)
   } catch (error) {
-    emitter.emit("log", `${error.name} ${error.message}`, "error-log", "\n");
-    console.log(error);
-    res.statusCode = 500;
-    res.end();
+    emitter.emit("log", `${error.name} ${error.message}`, "error-log", "\n")
+    console.log(error)
+    res.statusCode = 500
+    res.end()
   }
 }
 
 const server = http.createServer((req, res) => {
-  console.log("Server started");
+  console.log("Server started")
   emitter.emit(
     "log",
     `method: ${req.method}\troute: ${req.url}`,
     "req-log",
     "\n"
-  );
-  const extension = path.extname(req.url).slice(1);
-  const contentType = ext[extension] || ext.html;
-  const url = req.url;
-  let contentPath = null;
+  )
+  const extension = path.extname(req.url).slice(1)
+  const contentType = ext[extension] || ext.html
+  const url = req.url
+  let contentPath = null
 
   if (contentType === ext.html) {
-    if (url === "/") contentPath = filePath;
+    if (url === "/") contentPath = filePath
     else if (url.slice(-1) === "/")
-      contentPath = path.join(__dirname, "views", req.url, "index.html");
-    else contentPath = path.join(__dirname, "views", req.url);
-  } else contentPath = path.join(__dirname, req.url, "index.html");
+      contentPath = path.join(__dirname, "views", req.url, "index.html")
+    else contentPath = path.join(__dirname, "views", req.url)
+  } else contentPath = path.join(__dirname, req.url)
 
-  if (!extension && url.slice(-1) !== "/") contentPath += ".html";
+  if (!contentType && url.slice(-1) !== "/") contentPath += ".html"
 
-  const fileExist = existsSync(contentPath);
+  const fileExist = existsSync(contentPath)
 
   if (fileExist) {
-    serverFile(contentPath, contentType, res);
+    serverFile(contentPath, contentType, res)
   } else {
-    switch (path.parse(contentPath).base) {
+    switch (path.parse(contentPath).name) {
       case "old-page":
         res.writeHead(301, {
-          location: "/new-page.html",
-        });
-        res.end();
-        break;
+          location: "/new-page.html"
+        })
+        res.end()
+        break
       case "page":
         res.writeHead(301, {
-          location: "/",
-        });
-        res.end();
-        break;
+          location: "/"
+        })
+        res.end()
+        break
 
       default:
-        serverFile(path.join(__dirname, "views", "404.html"), ext.html, res);
+        serverFile(path.join(__dirname, "views", "404.html"), ext.html, res)
     }
   }
-  res.end();
-});
+  res.end()
+})
 
-server.listen(port);
-
-emitter.on("log", (msg) => {
-  logEvents(msg);
-});
+server.listen(port)
--- modules/log_events.js
+++ modules/log_events.js
@@ -1,29 +1,23 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-const path = require("path");
-const { appendFile } = require("fs/promises");
-const { existsSync, mkdirSync } = require("fs");
+const { format } = require("date-fns")
+const { v4: uuid } = require("uuid")
+const path = require("path")
+const { appendFile } = require("fs/promises")
+const { existsSync, mkdirSync } = require("fs")
 
-const logFile = path.join(
-  __dirname,
-  "..",
-  "content",
-  "logs",
-  "general_logs.txt"
-);
-const logPath = path.join(__dirname, "..", "content", "logs");
+const logPath = path.join(__dirname, "..", "content", "logs")
 
-async function logEvents(msg) {
-  const date = format(new Date(), "yyyyMMdd\tHH:mm:ss");
-  const log = date + " " + uuid() + " " + msg + "\n";
+async function logEvents(msg, logName) {
+  const logFile = path.join(__dirname, "..", "content", "logs", logName)
+  const date = format(new Date(), "yyyyMMdd\tHH:mm:ss")
+  const log = date + " " + uuid() + " " + msg + "\n"
 
-  if (!existsSync(logFile)) mkdirSync(logPath);
+  if (!existsSync(logPath)) mkdirSync(logPath)
 
   await appendFile(logFile, log, {
-    encoding: "utf8",
-  });
+    encoding: "utf8"
+  })
 }
 
 module.exports.logs = {
-  logEvents,
-};
+  logEvents
+}
